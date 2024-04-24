d45dba174e46704759fdd98e53559123758c832f
mikechao <mike.chao.one@gmail.com>
2024-04-24 10:04:23 -0700

    Basic setup

---
+++ src/main/java/dev/mike/chao/gtfsvptofile/GtfsVpToFileApplication.java
@@ -0,0 +1,13 @@
+package dev.mike.chao.gtfsvptofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtfsVpToFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtfsVpToFileApplication.class, args);
+	}
+
+}
